c655f8c571c0bc31cb05ede7f5e9b81e80df55ef
kame <tentyo0620@icloud.com>
2023-01-04 00:00:40 +0900

    Refactoring frontend

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script>
-	import Header from './Header.svelte';
-	import './styles.css';
+		import './styles.css';
 </script>
 
 <div class="app">
-	<Header />
 
 	<main>
 		<slot />
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,5 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
+
+
 
 <svelte:head>
 	<title>Home</title>
@@ -11,12 +8,6 @@
 
 <section>
 	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
 
 		to your new<br />SvelteKit app
 	</h1>
@@ -25,7 +16,6 @@
 		try editing <strong>src/routes/+page.svelte</strong>
 	</h2>
 
-	<Counter />
 </section>
 
 <style>
@@ -39,21 +29,5 @@
 
 	h1 {
 		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
