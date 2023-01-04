12f8e80d2a4e92b87465ccecf3d2880e1ad8c3c4
kame <tentyo0620@icloud.com>
2023-01-04 17:21:24 +0900

    Fixed directory structure

---
+++ frontend/src/core/domains/repository/TodoRepository.ts
@@ -0,0 +1,31 @@
+import type { Todo } from '../models/todo';
+
+const baseURL = 'http://localhost:8080';
+
+export class TodoRepository {
+	static async getAll(): Promise<Todo[]> {
+		const response = await fetch(baseURL + '/todos');
+		const todos: Todo[] = await response.json();
+		return todos || [];
+	}
+
+	static async addTodo(todo: Omit<Todo, 'id'>): Promise<void> {
+		await fetch(baseURL + 'todo/store', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(todo)
+		});
+	}
+
+	static async deleteTodo(id: Pick<Todo, 'id'>): Promise<void> {
+		await fetch(baseURL + 'todo/delete', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({ id: id })
+		});
+	}
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,8 +1,24 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+	import { TodoRepository } from '../core/domains/repository/TodoRepository';
+
+	export const load: Load = async () => {
+
+	}
+</script>
+
 <script lang="ts">
 	import TodoCard from '../component/TodoCard/index.svelte'
+	import { Input, Button } from 'flowbite-svelte'
 </script>
 
 <section>
+	<h2 class="mb-8 font-bold text-4xl">TODO</h2>
+	<form class="w-full mb-8">
+		<Input placeholder="テキストを入力してください" size="lg">
+			<Button slot="right" size="sm" type="submit">追加</Button>
+		</Input>
+	</form>
 	<TodoCard />
 </section>
 
